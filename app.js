e194a62c0add563eef5234dda6ddec9aaa96436f
Z8MB1E <jason@onyxsimple.com>
2022-10-26 00:45:14 -0400

    Add logfile command, add four logfiles

--- app.js
+++ app.js
@@ -91,6 +91,33 @@ const employees = [
   },
 ];
 
+const logFiles = [
+  {
+    fileName: "intern.1",
+    name: "Intern's Log (1/4)",
+    date: "02/22/2022",
+    data: [`I got in! I thought my interview was a bit shy of golden, but it doesn't matter now - SANCTUM INDUSTRIES clearly thought I had some kind of mettle when they hired me. I'm not sure what I'm going to do here, but I'm sure I'll find out soon enough.`, '', `I've heard rumors that there is some classified project that is finishing its last preliminary trials, and I might get to be a part of it. Classified! That's almost too cool. I guess it paid off to get that Master's in theoretical physics.`,'',`Well, let's get started!`],
+  },
+  {
+    fileName: "intern.2",
+    name: "Intern's Log (2/4)",
+    date: "03/15/2022",
+    data: [`No. Way. I still can't believe it... Not only am I assigned to the project, but I even get to work with the legendary Dr. Mendelssohn! He's, like, big time around here. Even the CEO is a bit intimidated by him, I think, though I suppose I can't blame him... how do you even begin to compete with a Nobel Prize winner?`, '', `I'm going to have to work hard to keep up with him, but I'm sure I'll be able to. I'm going to be a part of something big here, I just know it.`]
+  },
+  {
+    fileName: "intern.3",
+    name: "Intern's Log (3/4)",
+    date: "07/29/2022",
+    data: [`Project [REDACTED] is on schedule for testing in late October. I'm not sure how the results are going to compare to the preliminary trials, but if I learned anything from being around Dr. Mendelssohn, it's that I know nothing. Sure, I'm a theoretical physicist, but he's a Nobel Prize winner and the lab's top dog. I'm sure he knows what he's doing.`, '', `If I'm being honest, I'm not sure what I'm going to do after the project is over. I'm not sure I want to go back to school, but I'm not sure I want to stay here either. I guess I'll just have to wait and see. The Project doesn't wait for anyone, least of all me!`]
+  },
+  {
+    fileName: "intern.4",
+    name: "Intern's Log (4/4)",
+    date: "10/25/2022",
+    data: [`Dr. Mendelssohn has vanished. No one knows what happened to him, and every one of our questions to the higher-ups is answered the same way: "An investigation is underway. Keep working, stick to the timeline, and stay focused." Not the most encouraging thing to say to the people who have worked with Dr. Mendelssohn on the Project every day for the past year.`, '', `The Project was scheduled for testing on 10/██, but this development has thrown everything off-course. The CEO called for a mandatory all-hands and told us that the Board has arranged for several private investigation teams to look into the disappearance. I'm not sure what to think. I'm not sure what to do. I'm not sure what's going to happen next.`, '', `I just hope they find him.`]
+  }
+]
+
 async function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
@@ -433,7 +460,7 @@ const Commands = {
     },
   },
 
-  "projectx": {
+  projectx: {
     description: "[REDACTED]",
     usage: "projectx",
     run: async () => {
@@ -447,6 +474,51 @@ const Commands = {
         "PROJECT [REDACTED] IS A [REDACTED] PROJECT CURRENTLY IN DEVELOPMENT AT SANCTUM. THE PROJECT IS CURRENTLY IN ITS [REDACTED] PHASE.",
         "",
       ]);
-    }
+    },
   },
+
+  logfile: {
+    description: "Review one or more log files.",
+    usage: "logfile [name of file/all/blank]",
+    run: async (args) => {
+      if (args.length === 0 ) {
+        await Terminal.type([
+          "",
+          "ERROR: NO LOG FILE SPECIFIED. PLEASE SPECIFY A LOG FILE (EG. 'LOGFILE LOG.1').",
+          "AVAILABLE LOG FILES:",
+          "",
+        ]);
+        for (let i = 0; i < logFiles.length; i++) {
+          const log = logFiles[i];
+          await Terminal.type(`${log.fileName} - ${log.name}`, {
+            initialWait: 250,
+          });
+        }
+      } else {
+        const log = logFiles.find((log) => log.fileName === args[0]);
+        if (log) {
+          await Terminal.type([
+            "",
+            `${log.fileName} - ${log.name}`,
+            "======================",
+            "",
+          ]);
+          await Terminal.type([...log.data, ''], { lineWait: 1000, allCaps: false });
+        } else {
+          await Terminal.type([
+            "",
+            "ERROR: INVALID LOG FILE. PLEASE SPECIFY A VALID LOG FILE (EG. 'LOGFILE LOG.1').",
+            "AVAILABLE LOG FILES:",
+            "",
+          ]);
+          for (let i = 0; i < logFiles.length; i++) {
+            const log = logFiles[i];
+            await Terminal.type(`${log.fileName} - ${log.name}`, {
+              initialWait: 250,
+            });
+          }
+        }
+      }
+    }
+  }
 };
